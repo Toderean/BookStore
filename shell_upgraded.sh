466d09bce55f21120e736bef8c4de0a82e079b0c
Toderean <todereanrazvan@yahoo.com>
2024-03-13 16:21:31 +0200

    Update shell_upgraded.sh

--- shell_upgraded.sh
+++ shell_upgraded.sh
@@ -1,34 +1,32 @@
-    last_commit=$(git log -1 --pretty=format:"%h")
-    full_path=$(git diff-tree --no-commit-id --name-only -r ${last_commit})
-    changes_path=$(dirname "$full_path" | awk -F'/' '{print $1"/"$2}')
-    package_types=("package.json" "Cargo.toml")
-    package=""
-
-    for dir_path in ${changes_path}; do
-        echo $dir_path 
-        temp=$(echo "$dir_path" | awk -F'/' '{print $1}')
-        echo $temp
-        case $package_type in
-            "packages" )
-                full_path=$changes_path
-                ;;
-            *)
-                continue
-                ;;
-        esac                
-    done
-    
-    cd $dir_path
-    
-    files=$(ls)
-    
-    for package_type in ${package_types[@]}; do
-            for file in ${files[@]}; do
-            if [ "${package_type}" == "${file}" ]; then
-                package=${package_type}
-                echo $package
-            fi
-            done    
-    done
-full_path_with_package="${full_path}/${package}"
-echo "FULL_PATH=$full_path_with_package" >> $GITHUB_ENV
+            last_commit="95c86a9f1785ba92228b4b18a453010d6efc4eeb"
+            full_path=$(git diff-tree --no-commit-id --name-only -r ${last_commit})
+            changes_path=$(dirname "$full_path" | awk -F'/' '{print $1"/"$2}')
+            package_types=("package.json" "Cargo.toml")
+            package=""
+        
+            for dir_path in ${changes_path}; do
+                echo $dir_path 
+                temp=$(echo "$dir_path" | awk -F'/' '{print $1}')
+                echo $temp
+                case $temp in
+                    "packages" )
+                        full_path=$changes_path
+                        ;;
+                    *)
+                        continue
+                        ;;
+                esac                
+            done
+            
+            cd $dir_path
+            
+            files=$(ls)
+            
+            for package_type in ${package_types[@]}; do
+                    for file in ${files[@]}; do
+                    if [ "${package_type}" == "${file}" ]; then
+                        package=${package_type}
+                        echo $package
+                    fi
+                    done    
+            done
